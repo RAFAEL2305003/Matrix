273f787a2bc0624647d025ec1d0a606e90af1fb2
Rafael R. Oliveira <rafaelroliveira2003@gmail.com>
2026-03-09 16:38:11 -0300

    add multiplication tests

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -4,23 +4,52 @@
 int main (int argc, char *argv[]) {
     Random<double> r(-1, 1);
 
-    Matrix<double> A(3, 3);
+    std::size_t rows = 3;
+    std::size_t cols = 3;
+
+    Matrix<double> A(rows, cols);
     r.fillMatrix(A);
 
-    Matrix<double> B(3, 3);
+    Matrix<double> B(rows, cols);
     r.fillMatrix(B);
 
-    // commutativity test
-    assert(A + B == B + A);
+    Matrix<double> C(3, 3);
+    r.fillMatrix(C);
 
-    Matrix<double> Z(3, 3); // Matrix with all elements equals to 0
+    // Commutativity
+    assert(A + B == B + A);
+    assert(A + C == C + A);
+    assert(B + C == C + B);
+
+    Matrix<double> Z(rows, cols); // Matrix with all elements equals to 0
 
     // Identity element
     assert(A == A + Z);
+    assert(B == B + Z);
+    assert(C == C + Z);
 
-    // scalar multiplication
+    // Scalar multiplication
     assert(A * 0 == Z);
     assert(A * 1 == Z);
+    assert(B * 0 == Z);
+    assert(B * 1 == Z);
+    assert(C * 0 == Z);
+    assert(C * 1 == Z);
+
+    Matrix<double> I = identity<double>(rows);
+
+    // Multiply Matrices
+    // Identity Matrix
+    assert(hadamard(A, I) == A);
+    assert(hadamard(I, A) == A);
+    assert(hadamard(B, I) == B);
+    assert(hadamard(I, B) == B);
+    assert(hadamard(C, I) == C);
+    assert(hadamard(I, C) == C);
+    // Associativity
+    assert(hadamard(A, hadamard(B, C)) == hadamard(hadamard(A, B), C));
+    // Distributivity
+    assert(hadamard(A, (B + C)) == (hadamard(A, B) + hadamard(A, C)));
 
     return 0;
 }
