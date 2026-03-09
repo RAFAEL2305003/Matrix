6ac1a91d9f189103a479c1e0ca4e78c28188eeb8
Rafael R. Oliveira <rafaelroliveira2003@gmail.com>
2026-03-09 17:04:05 -0300

    add more tests

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -1,6 +1,8 @@
 #include "../include/matrix.h"
 #include "../include/random.h"
 
+// TODO: Add Stress Tests
+
 int main (int argc, char *argv[]) {
     Random<double> r(-1, 1);
 
@@ -10,10 +12,10 @@ int main (int argc, char *argv[]) {
     Matrix<double> A(rows, cols);
     r.fillMatrix(A);
 
-    Matrix<double> B(rows, cols);
+    Matrix<double> B(cols, rows);
     r.fillMatrix(B);
 
-    Matrix<double> C(3, 3);
+    Matrix<double> C(rows, cols);
     r.fillMatrix(C);
 
     // Commutativity
@@ -50,6 +52,33 @@ int main (int argc, char *argv[]) {
     assert(hadamard(A, hadamard(B, C)) == hadamard(hadamard(A, B), C));
     // Distributivity
     assert(hadamard(A, (B + C)) == (hadamard(A, B) + hadamard(A, C)));
+
+    // Transpose properties
+    // Double transpose
+    assert(A.transpose().transpose() == A);
+    assert(B.transpose().transpose() == B);
+    assert(C.transpose().transpose() == C);
+    // Transpose of product
+    assert(hadamard(A, B).transpose() == hadamard(A.transpose(), B.transpose()));
+    assert(hadamard(A, C).transpose() == hadamard(A.transpose(), C.transpose()));
+    assert(hadamard(B, C).transpose() == hadamard(B.transpose(), C.transpose()));
+    // Transpose of sum
+    assert((A + B).transpose() == A.transpose() + B.transpose());
+    assert((A + C).transpose() == A.transpose() + C.transpose());
+    assert((B + C).transpose() == B.transpose() + C.transpose());
+
+    rows = 5;
+    cols = 2;
+    std::size_t rows_ = 2;
+    std::size_t cols_ = 6;
+
+    Matrix<double> X(rows, cols);
+    Matrix<double> Y(rows_, cols_);
+
+    // Dimensions Test
+    auto [rows__, cols__] = hadamard(X, Y).shape();
+    assert(rows__ == rows && cols__ == cols_);
+    
 
     return 0;
 }
